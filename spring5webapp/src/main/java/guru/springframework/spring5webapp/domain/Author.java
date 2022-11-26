852e149555ae3be7b9eb41cf3092926fc13e9304
pdas9647 <padmanabhadas9647@gmail.com>
2022-11-26 22:47:13 +0530

    Section 2 completed
    Signed-off-by: pdas9647 <padmanabhadas9647@gmail.com>

---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,121 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String firstName;
+
+	private String lastName;
+
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books = new HashSet<> ( );
+
+	/**
+	 *
+	 */
+	public Author() {
+	}
+
+	/**
+	 * @param firstName
+	 * @param lastName
+	 * @param books
+	 */
+	public Author(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the books
+	 */
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	/**
+	 * @param books the books to set
+	 */
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash ( id );
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Author)) {
+			return false;
+		}
+		Author other = (Author) obj;
+		return Objects.equals ( id, other.id );
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+}
