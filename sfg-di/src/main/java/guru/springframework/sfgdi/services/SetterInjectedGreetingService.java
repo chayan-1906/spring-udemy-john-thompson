45cab2f0636bab66665f5620b0c8ffc5fd81caa2
pdas9647 <padmanabhadas9647@gmail.com>
2022-11-27 12:23:12 +0530

    Section 3 Video 36 completed

---
+++ sfg-di/src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgdi.services;
+
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetterInjectedGreetingService implements GreetingService {
+	/**
+	 * @return
+	 */
+	@Override
+	public String sayGreeting() {
+		return "Hello World - Setter";
+	}
+}
