dd98f5f5ef64007f2ff44d3ee398b40d1d5d9e0e
pdas9647 <padmanabhadas9647@gmail.com>
2022-12-08 11:52:35 +0530

    Section 5 completed
    Co-Authored-By: Padmanabha Das <82430454+chayan-1906@users.noreply.github.com>

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,12 @@
 package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Pet;
-import guru.springframework.sfgpetclinic.services.CrudService;
 import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 	/**
 	 * @return
@@ -46,6 +47,6 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
 	 */
 	@Override
 	public Pet save(Pet pet) {
-		return super.save ( pet.getId ( ), pet );
+		return super.save ( pet );
 	}
 }
--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,12 @@
 package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Vet;
-import guru.springframework.sfgpetclinic.services.CrudService;
 import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 	/**
 	 * @return
@@ -46,6 +47,6 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 	 */
 	@Override
 	public Vet save(Vet vet) {
-		return super.save ( vet.getId ( ), vet );
+		return super.save ( vet );
 	}
 }
